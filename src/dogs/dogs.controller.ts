5f64e399f5ae1aeb790ff252bc1389ff825bfff9
=basantaisg <basantaparajuli.11@gmail.com>
2025-01-19 16:36:53 +0545

    commit 2

--- src/dogs/dogs.controller.ts
+++ src/dogs/dogs.controller.ts
@@ -9,19 +9,23 @@ import {
 } from '@nestjs/common';
 import { CreateDogDto } from './dto/create-dog.dto';
 import { UpdateDogBreed } from './dto/update-dog.dto';
+import { DogsService } from './dogs.service';
+import { Dog } from './interfaces/dogs.interface';
 
 @Controller('dogs')
 export class DogsController {
+  constructor(private dogsService: DogsService) {}
+
   @Post()
   // @Header('Token', 'hskljhdsakfhljasdhklf')
   // @HttpCode(201)
-  create(@Body() dogInformation: CreateDogDto) {
-    return dogInformation;
+  async create(@Body() dogInformation: CreateDogDto) {
+    return this.dogsService.create(dogInformation);
   }
 
   @Get()
-  findAll() {
-    return [];
+  async findAll(): Promise<Dog[]> {
+    return this.dogsService.findAll();
   }
 
   @Get('breed')
--- src/dogs/dogs.service.ts
+++ src/dogs/dogs.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Dog } from './interfaces/dogs.interface';
 
 @Injectable()
-export class DogsService {}
+export class DogsService {
+  private readonly dogsInfo: Dog[] = [];
+
+  create(dog: Dog) {
+    this.dogsInfo.push(dog);
+  }
+
+  findAll(): Dog[] {
+    return this.dogsInfo;
+  }
+}
