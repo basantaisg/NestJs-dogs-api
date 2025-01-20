163c50c652fe3891f66c2eac70e646a660447133
=basantaisg <basantaparajuli.11@gmail.com>
2025-01-20 18:51:41 +0545

    middlewares update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,37 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { DogsModule } from './dogs/dogs.module';
+// import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { DogsController } from './dogs/dogs.controller';
+import { logger } from './middlewares/logger.middleware';
 
 @Module({
   imports: [DogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  // configure(consumer: MiddlewareConsumer) {
+  //   consumer
+  //     .apply(LoggerMiddleware)
+  //     .exclude({
+  //       path: 'dogs/breed/1',
+  //       method: RequestMethod.GET,
+  //     })
+  //     .forRoutes(DogsController);
+  // }
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(logger)
+      .exclude({ path: '/dogs/breed/1', method: RequestMethod.GET })
+      .forRoutes(DogsController);
+  }
+}
